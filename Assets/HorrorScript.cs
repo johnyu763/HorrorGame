2325d8b1f21abe2d0f2bd1794eac0e5eac684c8c
johnyu763 <johnyu763@gmail.com>
2020-02-02 02:37:11 -0800

    Merge attempt #2

---
+++ Assets/HorrorScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorrorScript : MonoBehaviour
+{
+    public Material mat;
+ 
+
+    void Start()
+    {
+
+        RenderSettings.skybox = mat;
+
+    }
+}

3bcd457c98a6aacbb865bb8efa73b1002ec5f121
johnyu763 <johnyu763@gmail.com>
2020-02-01 20:35:57 -0800

    I don't remember

---
+++ Assets/MissionControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionControl : MonoBehaviour
+{
+    private Animator anim;
+    private int stateNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.N))
+        {
+            StartCoroutine(nextState());
+        }
+    }
+
+    IEnumerator nextState()
+    {
+        anim.SetInteger("stateNum", ++stateNum);
+        yield return new WaitForSeconds(10f);
+    }
+}
